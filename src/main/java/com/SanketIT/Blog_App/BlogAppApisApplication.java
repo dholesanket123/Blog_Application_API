fe0a2f6add094de6a36b4b2ae3720b7055385e09
dholesanket123 <sanketdhole133@gmail.com>
2022-06-21 20:09:34 +0530

    BBA-2:Create spring boot Application and push this github

---
+++ src/main/java/com/SanketIT/Blog_App/BlogAppApisApplication.java
@@ -0,0 +1,13 @@
+package com.SanketIT.Blog_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAppApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogAppApisApplication.class, args);
+	}
+
+}
